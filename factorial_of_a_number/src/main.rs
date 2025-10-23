b0fb1d7b213c83ec0410fb2da13918d555fe632f
Yoshishige Unose <966.krr@gmail.com>
2025-10-23 15:37:18 +0900

    add err handler

---
+++ factorial_of_a_number/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use std::io::Write;
+
+fn main() {
+    let mut input = String::new();
+
+    loop {
+        print!("Input a number to get factorial! ");
+        io::stdout()
+            .flush()
+            .unwrap();
+
+        io::stdin()
+            .read_line(&mut input)
+            .unwrap();
+
+        match input.trim_end().parse::<u128>() {
+            Ok(1..35) => break,
+            _ => {
+                input.clear();
+                println!("[Invalid input]\nOnly supports a natural number between 1 and 34 due to memory overflow...");
+            },
+        }
+    }
+
+    let input = input.trim_end().parse::<u128>().unwrap();
+    let mut product: u128 = input;
+
+    for n in (1..input).rev() {
+        product *= n;
+    }
+
+    print!("factorial of {input} ({input}!) is: {product}");
+}
