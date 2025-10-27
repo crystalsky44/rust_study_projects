8d8350860e63b54b28217aa106b60c6a0be73a9b
Yoshishige Unose <966.krr@gmail.com>
2025-10-27 18:14:42 +0900

    add fn main(), fn truncate_at_hundreth(foo: f32)\n just by intuition, this code already is in need of refactoring. I think...

--- b_currency_converter/src/main.rs
+++ b_currency_converter/src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    print!("input a number! ");
+
+    let usd_eur_rate: f32 = 0.86;
+
+    let mut input = String::new();
+
+    io::stdout()
+        .flush()
+        .unwrap();
+
+    io::stdin()
+        .read_line(&mut input)
+        .unwrap();
+
+    let input = input.trim_end().parse::<f32>().unwrap() * usd_eur_rate;
+
+    print!("your input: {input}");
+
+}
+
+fn truncate_at_hundreth(floating_point: &mut f32) -> f32 {
+    let truncated: f32 = (*floating_point * 100.0).trunc() / 100.0;
+    truncated
 }
