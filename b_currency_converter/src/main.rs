fb32bcc1c0e6818d4b2ce4b6a61dcc21a3352fb9
Yoshishige Unose <966.krr@gmail.com>
2025-10-29 10:06:43 +0900

    add data type [UserMoney] to implement a created method which operates on the primitive type f32.\n Having difficulty implementing the method and std::ops::Mul trait...

--- b_currency_converter/src/main.rs
+++ b_currency_converter/src/main.rs
@@ -1,28 +1,40 @@
 use std::io;
 use std::io::Write;
 
+struct UserMoney(f32);
+
+impl UserMoney {
+    fn truncate_after_hundreth(&mut self) {
+        *self = (*self * 100.0).trunc() / 100.0;
+    }
+}
+
 fn main() {
-    print!("input a number! ");
 
     let usd_eur_rate: f32 = 0.86;
-
     let mut input = String::new();
+
+    user_input(&mut input);
+
+    let mut user_money = UserMoney(input.trim_end().parse::<f32>().unwrap());
+    // truncate_after_hundreth(&mut user_money);
+
+    let mut result = UserMoney(user_money.truncate_after_hundreth() * usd_eur_rate);
+    result.truncate_after_hundreth();
+    // truncate_after_hundreth(&mut result);
+
+    print!("your input: {result:?}");
+
+}
+
+fn user_input(input: &mut String) {
+    print!("input a number! ");
 
     io::stdout()
         .flush()
         .unwrap();
 
     io::stdin()
-        .read_line(&mut input)
+        .read_line(input)
         .unwrap();
-
-    let input = input.trim_end().parse::<f32>().unwrap() * usd_eur_rate;
-
-    print!("your input: {input}");
-
-}
-
-fn truncate_at_hundreth(floating_point: &mut f32) -> f32 {
-    let truncated: f32 = (*floating_point * 100.0).trunc() / 100.0;
-    truncated
 }
