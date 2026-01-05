678fcb7327d0ec5862f1652247cd8cfb993a7a11
Yoshishige Unose <966.krr@gmail.com>
2026-01-05 16:55:53 +0900

    update

--- c_todo_using_json/src/lib.rs
+++ c_todo_using_json/src/lib.rs
@@ -1,4 +1,5 @@
 use serde::{Serialize, Deserialize};
+use anyhow::Result;
 
 use std::fs::{File, OpenOptions};
 use std::io;
@@ -16,49 +17,47 @@ pub struct Task {
 }
 
 impl Task {
-    pub fn new(task_title: String) {
-        let task = task_input(task_title);
-        let task_path = "task.json"; // for now, appneding to a specific file
+    pub fn new(title: String) -> Task {
+        let description = get_user_input("input a description:");
 
-        match File::create_new(task_path) {
-            Ok(_) => write_task_to_file(task_path, &vec![task]),
-            Err(_) => append_task(task_path, task),
+        Task {
+            title,
+            description,
+        }
+    }
+
+    pub fn store(self, task_path: impl AsRef<Path>) -> Result<()> { 
+        match File::create_new(&task_path) {
+            Ok(_) => write_task_to_file(task_path, &vec![self])?,
+            Err(_) => append_task(task_path, self)?,
         }
 
+        Ok(())
     }
 }
 
-fn append_task(path: impl AsRef<Path>, task: Task) {
-    let file = File::open(&path).unwrap();
+fn append_task(path: impl AsRef<Path>, task: Task) -> Result<()> {
+    let file = File::open(&path)?;
     let reader = BufReader::new(file);
 
-    let mut task_vec: Vec<Task> = serde_json::from_reader(reader).unwrap();
+    let mut task_vec: Vec<Task> = serde_json::from_reader(reader)?;
     task_vec.push(task);
 
-    write_task_to_file(&path, &task_vec);
+    write_task_to_file(&path, &task_vec)
 }
 
-fn write_task_to_file(path: impl AsRef<Path>, task_vec: &Vec<Task>) {
+fn write_task_to_file(path: impl AsRef<Path>, task_vec: &Vec<Task>) -> Result<()> {
     let json_file = OpenOptions::new()
         .write(true)
         .truncate(true)
-        .open(path)
-        .unwrap();
+        .open(path)?;
 
     let writer = BufWriter::new(json_file);
-    serde_json::to_writer_pretty(writer, &task_vec).unwrap();
+    serde_json::to_writer_pretty(writer, &task_vec)?;
+    Ok(())
 }
 
-fn task_input(title: String) -> Task {
-    let description = input("input a description:");
-
-    Task {
-        title,
-        description,
-    }
-}
-
-fn input(field: &str) -> String {
+fn get_user_input(field: &str) -> String {
     print!("{field} ");
 
     let mut input = String::new();
--- c_todo_using_json/src/main.rs
+++ c_todo_using_json/src/main.rs
@@ -1,14 +1,70 @@
 use std::env;
 use c_todo_using_json::Task;
+use anyhow::Result;
 
+// let's try implementing below main advised from ChatGPT
+// make sure ask "why" the main might be more idiomatic than one above
+fn main() {
+    match run() {
+        Ok(_) => println!("Task added successfully!"),
+        Err(_) => println!("some error") 
+    }
+}
+
+fn run() -> Result<()> {
+    let title = env::args().nth(1).ok_or(anyhow::Error)?;
+    let task = Task::new(title);
+
+    task.store("task.json")?;
+
+    Ok(())
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// dump of old codes below, delete it when I'm sure I'm never coming back to it
+
+/*
 fn main() {
     Task::new(env::args().nth(1).unwrap());
 
     println!("Task loaded!");
 }
+*/
 
 /*
 fn app() -> /* some kind of Result (maybe use thiserror)*/{
     // some code 
 }
 */
+
+/*
+mod another_way {
+    fn main() {
+        // one thing I still wondering, is what do I put in main?
+        let arg = env::args().nth(1).unwrap();
+        // ideal for above probably is let arg = env::arg().nth(1)?;
+        
+        let task: Task = Task::new(arg); // this create a "Task" data,
+                                         // which is NOT like the one above line
+
+        append_task(task); // this appends task to file, but this also is
+                           // different than the two lines above
+    }
+}
+*/
+
+
